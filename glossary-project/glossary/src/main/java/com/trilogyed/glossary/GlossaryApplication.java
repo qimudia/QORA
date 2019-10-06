17c5d0c05511838936136deddafcf9c961ff5d6c
ace <allisterg@gmail.com>
2019-10-06 11:34:38 -0400

    add feign to pom, add controller, add definition model, add definition feign client

--- glossary-project/glossary/src/main/java/com/trilogyed/glossary/GlossaryApplication.java
+++ glossary-project/glossary/src/main/java/com/trilogyed/glossary/GlossaryApplication.java
@@ -2,9 +2,11 @@ package com.trilogyed.glossary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-public class GlossaryApplication {
+@EnableFeignClients
+public class 	GlossaryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GlossaryApplication.class, args);
--- glossary-project/glossary/src/main/java/com/trilogyed/glossary/controller/GlossaryController.java
+++ glossary-project/glossary/src/main/java/com/trilogyed/glossary/controller/GlossaryController.java
@@ -1,4 +1,25 @@
 package com.trilogyed.glossary.controller;
 
+import com.trilogyed.glossary.model.Definition;
+import com.trilogyed.glossary.util.feign.DefinitionClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/glossary")
 public class GlossaryController {
+
+    @Autowired
+    DefinitionClient client;
+
+    @GetMapping("/term/{term}")
+    public List<Definition> getDefinitionsByTerm(@PathVariable String term) {
+        return client.getDefinitionsFromService(term);
+    }
+
 }
