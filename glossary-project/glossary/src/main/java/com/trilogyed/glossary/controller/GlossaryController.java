f57d9f4b48299e677c949022e917284e108377d2
Queen <youngidia@live.com>
2019-10-09 19:22:58 -0400

    Project

--- glossary-project/glossary/src/main/java/com/trilogyed/glossary/controller/GlossaryController.java
+++ glossary-project/glossary/src/main/java/com/trilogyed/glossary/controller/GlossaryController.java
@@ -1,7 +1,10 @@
 package com.trilogyed.glossary.controller;
 
 
+import com.trilogyed.glossary.util.feign.DefinitionServiceClient;
+import org.aspectj.weaver.loadtime.definition.Definition;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- glossary-project/glossary/src/main/java/com/trilogyed/glossary/util/feign/DefinitionServiceClient.java
+++ glossary-project/glossary/src/main/java/com/trilogyed/glossary/util/feign/DefinitionServiceClient.java
@@ -1,5 +1,14 @@
 package com.trilogyed.glossary.util.feign;
 
-public interface DefinitionServiceClient {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+    public interface DefinitionServiceClient {
+
+
+
 
 }
